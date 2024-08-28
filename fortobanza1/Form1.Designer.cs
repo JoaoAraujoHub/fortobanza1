48b2458d8de4b0614031837ee0ec2266a90b835a
JoaoAraujoHub <araujo.joao1@aluno.ifsp.edu.br>
2024-08-28 12:19:00 -0300

    João

--- fortobanza1/Form1.Designer.cs
+++ fortobanza1/Form1.Designer.cs
@@ -29,61 +29,90 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btmAvançar = new System.Windows.Forms.Button();
+            this.txbPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbUser = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.DarkBlue;
-            this.label1.Font = new System.Drawing.Font("Swis721 BlkEx BT", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Swis721 BlkEx BT", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Snow;
-            this.label1.Location = new System.Drawing.Point(262, 72);
+            this.label1.Location = new System.Drawing.Point(241, 65);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(340, 25);
+            this.label1.Size = new System.Drawing.Size(385, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Crie sua conta breezy";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
+            // panel1
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.textBox1.Location = new System.Drawing.Point(266, 158);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(234, 29);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "Endereço E-mail";
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.btmAvançar);
+            this.panel1.Controls.Add(this.txbPass);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txbUser);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(262, 130);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(339, 273);
+            this.panel1.TabIndex = 1;
             // 
-            // textBox2
+            // btmAvançar
             // 
-            this.textBox2.Location = new System.Drawing.Point(266, 193);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(217, 20);
-            this.textBox2.TabIndex = 2;
+            this.btmAvançar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btmAvançar.Location = new System.Drawing.Point(232, 221);
+            this.btmAvançar.Name = "btmAvançar";
+            this.btmAvançar.Size = new System.Drawing.Size(81, 39);
+            this.btmAvançar.TabIndex = 4;
+            this.btmAvançar.Text = "Avançar";
+            this.btmAvançar.UseVisualStyleBackColor = true;
+            this.btmAvançar.Click += new System.EventHandler(this.btmAvançar_Click);
+            this.btmAvançar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // txbPass
+            // 
+            this.txbPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbPass.Location = new System.Drawing.Point(22, 152);
+            this.txbPass.Name = "txbPass";
+            this.txbPass.Size = new System.Drawing.Size(274, 24);
+            this.txbPass.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(22, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Senha";
+            // 
+            // txbUser
+            // 
+            this.txbUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbUser.Location = new System.Drawing.Point(22, 61);
+            this.txbUser.Name = "txbUser";
+            this.txbUser.Size = new System.Drawing.Size(274, 24);
+            this.txbUser.TabIndex = 1;
+            this.txbUser.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.White;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(263, 258);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(17, 21);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 24);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Senha";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(266, 285);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(217, 20);
-            this.textBox3.TabIndex = 4;
+            this.label2.Size = new System.Drawing.Size(78, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "E-mail";
             // 
             // Form1
             // 
@@ -92,14 +121,13 @@
             this.BackColor = System.Drawing.Color.Gray;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,10 +136,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txbUser;
+        private System.Windows.Forms.Button btmAvançar;
+        private System.Windows.Forms.TextBox txbPass;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- fortobanza1/Form1.cs
+++ fortobanza1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,14 +18,45 @@ namespace fortobanza1
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void label2_Click(object sender, EventArgs e)
         {
 
         }
-    }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            var result = MessageBox.Show("E-mail: " + txbUser.Text + "\n" + "Senha: " + txbPass.Text, "Informações digitadas",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+            if (txbUser.Text == "joão" && txbPass.Text == "123")
+            {
+                txbUser.Text = string.Empty; //Limpa o campo de usuario 
+                txbPass.Text = string.Empty; //Limpa o campo de senha
+                txbUser.Focus(); //Coloca o foco no campo de usuario
+                Form2 Form2 = new Form2();
+                this.Visible = false; //Esconder tela de login
+                Form2.ShowDialog();
+                this.Visible = true;  //Voltar a mostrar a tela de login
+            }
+            else
+            {
+                MessageBox.Show("Usuario e/ou senha incorretos",
+                         "ERRO NO LOGIN",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error);
+                txbUser.Text = string.Empty; //Limpa o campo de usuario 
+                txbPass.Text = string.Empty; //Limpa o campo de senha
+                txbUser.Focus(); //Coloca o foco no campo de usuario
+            }
+        }
+
+        private void btmAvançar_Click(object sender, EventArgs e)
+        {
+        }
+    }   
 }
